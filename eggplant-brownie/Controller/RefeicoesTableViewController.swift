be25ffc9250b8ab826a956830c3e4430e86ab1e8
Breno <cintrabreno22@gmail.com>
2020-12-18 14:51:25 -0300

    adicionado Table View no projeto

---
+++ eggplant-brownie/Controller/RefeicoesTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  RefeicoesTableViewController.swift
+//  eggplant-brownie
+//
+//  Created by Breno Luizetto Cintra on 18/12/20.
+//  Copyright © 2020 Alura. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RefeicoesTableViewController: UITableViewController{
+    
+    let refeicoes = [Refeicao(nome: "macarrao", felicidade: 4),
+                     Refeicao(nome: "arroz", felicidade: 3),
+                    Refeicao(nome: "feijao", felicidade: 5)]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return refeicoes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        let refeicao = refeicoes [indexPath.row]
+        
+        celula.textLabel?.text = refeicao.nome
+        
+        return celula
+    }
+}
